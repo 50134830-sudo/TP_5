90aebe83276b8383bf89c3c43bca050fe9b31fb6
50134830-sudo <50134830@est.ort.edu.ar>
2026-04-13 14:31:27 -0300

    Interactive Buttons
    a

---
+++ Assets/scripts/UI_Password_Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_Password_Manager : MonoBehaviour
+{
+    public GameObject txtResultadoPasswd;
+    public TextMeshProUGUI  txtResultadoPasswdTexto;
+    public TMP_InputField input_Password;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtResultadoPasswd.SetActive(false);
+    }
+
+   public  void ValidatePassword()
+    {
+        txtResultadoPasswd.SetActive(true);
+        txtResultadoPasswdTexto.text = input_Password.text;
+    }
+
+
+}
+
